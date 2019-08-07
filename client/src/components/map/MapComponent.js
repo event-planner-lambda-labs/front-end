9ea816332e12e67b3f1635ea61ac550257df10a2
Oliver-Strange <preston.burton@gmail.com>
2019-08-07 14:08:21 -0700

    installs react-places-autocomplete

--- client/src/components/map/MapComponent.js
+++ client/src/components/map/MapComponent.js
@@ -4,7 +4,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "reac
 //import * as eventsData from "data file for event details";
 import mapStyles from "../../styles/MapStyles";
 import Geo from "./GeoComponent";
-// import LocationSearchComponent from "./LocationSearchComponent";
+import LocationSearchComponent from "./LocationSearchComponent";
 
 function Map() {
   //setting up to display event detail boxes when clicking markers
@@ -13,21 +13,48 @@ function Map() {
   //function for geolocation unsure if correct in usage here, may be able to implement without making it a function?
 
   //default map renders to ground 0, eventsData and id can be confirmed/updated once event dataset array is built out
+  const dummyData = [
+    {
+      lat: 39.063555,
+      lng: -94.583268
+    },
+    {
+      lat: 39.95233,
+      lng: -75.16379
+    },
+    {
+      lat: 39.045,
+      lng: -94.586
+    },
+    {
+      lat: 39.0073,
+      lng: -94.5295
+    },
+    {
+      lat: 39.463598,
+      lng: -94.583232
+    },
+    {
+      lat: 39.263559,
+      lng: -94.583252
+    }
+  ];
+
   return (
     <body className="google-map">
       <Geo />
-      {/* <LocationSearchComponent /> */}
+      <LocationSearchComponent />
       <GoogleMap
-        defaultZoom={10}
+        defaultZoom={18}
         defaultCenter={{ lat: 39.063555, lng: -94.583268 }}
         defaultOptions={{ styles: mapStyles }}
       >
-        {/* {eventsData.features.map(events => (
+        {dummyData.map(events => (
           <Marker
-            key={events.properties.EVENT_ID}
+            key={events.length}
             position={{
-              lat: events.geometry.coordinates[1],
-              lng: events.geometry.coordinates[0]
+              lat: events.lat, // must be an integer, not a string
+              lng: events.lng
             }}
             //onclick event to show event details when clicking marker
             onClick={() => {
@@ -35,11 +62,11 @@ function Map() {
             }}
             //displays icon for event, using default icon for now until category icons are integrated
             icon={{
-              url: "/event-icon.png",
+              url: "http://prankster101.com/newsite/wp-content/uploads/event-icon.png",
               scaledSize: new window.google.maps.Size(25, 25)
             }}
           />
-        ))} */}
+        ))}
 
         {selectedEvent && (
           //displays data from database based on selected park
@@ -70,7 +97,7 @@ const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 export default function eventMap() {
   return (
-    <div style={{ width: "50vw", height: "50vh" }}>
+    <div style={{ width: "100vw", height: "90vh" }}>
       <WrappedMap
         googleMapURL={
           "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCtTDf-cRIv4LFjrPKPUttpcLXb45tHhRw 	"
