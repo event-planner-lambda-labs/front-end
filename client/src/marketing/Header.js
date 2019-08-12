370e9c6c738f70df9f91758ecc83d9584b645256
atolmie <atolmie22@gmail.com>
2019-08-12 16:32:35 -0500

    landing starter

---
+++ client/src/marketing/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+  render() {
+    return (
+     
+      <header className="landingHeader">
+
+        <div className="head">
+            <h1>Go Have Fun</h1>
+            <div>
+              <p>Integer posuere leo non erat ornare dictum id vitae magna. Proin consectetur iaculis nisi, ut convallis tortor tempor congue. Curabitur sit amet tempus felis. Duis tellus eros, pellentesque at rhoncus eu, maximus ut diam.</p>
+          <button className="contact"Contact>Start Having Fun Now</button>
+            </div>
+        </div>
+       
+      </header>
+      
+    );
+  }
+}
+
+export default Header;
