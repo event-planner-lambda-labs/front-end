dd31ee5ad4027fc14440ab6dea91054e54192cb7
Oliver-Strange <preston.burton@gmail.com>
2019-08-14 09:39:08 -0700

    adds fontawesome github icons, fixes styling of logo, adds functionality to go to events button

--- client/src/marketing/Header.js
+++ client/src/marketing/Header.js
@@ -1,24 +1,33 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
 class Header extends Component {
+  btnClicked = path => {
+    this.props.history.push(`/${path}`);
+    setTimeout(() => {
+      this.setState({ isOpen: false });
+    }, 500);
+  };
+
   render() {
     return (
-     
       <header className="landingHeader">
-
         <div className="head">
-            <h1>Go Have Fun</h1>
-            <div>
-              <p>Integer posuere leo non erat ornare dictum id vitae magna. Proin consectetur iaculis nisi, ut convallis tortor tempor congue. Curabitur sit amet tempus felis. Duis tellus eros, pellentesque at rhoncus eu, maximus ut diam.</p>
-          <button className="contact"Contact>FIND AN EVENT</button>
-            </div>
+          <h1>Go Have Fun</h1>
+          <div>
+            <p>
+              Integer posuere leo non erat ornare dictum id vitae magna. Proin consectetur iaculis
+              nisi, ut convallis tortor tempor congue. Curabitur sit amet tempus felis. Duis tellus
+              eros, pellentesque at rhoncus eu, maximus ut diam.
+            </p>
+            <button className="contact" onClick={() => this.btnClicked("main")}>
+              FIND AN EVENT
+            </button>
+          </div>
         </div>
-       
       </header>
-      
     );
   }
 }
 
-export default Header;
+export default withRouter(Header);
