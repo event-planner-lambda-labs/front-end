6f10c67cd5fae5ff7bffea4bca051b3a199c3eed
mdelzotto <madelzotto@comcast.net>
2019-08-20 15:42:42 -0400

    Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,21 +7,12 @@ class Footer extends Component {
       <footer>
         <p>
           © 2019
-          <a href="">About</a>
-          <a href="">Contact</a>
           <a href="https://github.com/event-planner-lambda-labs">GitHub</a>
-          <a href="https://github.com/event-planner-lambda-labs/front-end/blob/master/LICENSE">
-            License
-          </a>
           <a href="https://lambdaschool.com/">Lambda School</a>
-          <a href="https://twitter.com">twitter</a>
-          <a href="https://www.facebook.com">facebook</a>
-          <a href="https://www.instagram.com">
-            <i class="fab fa-instagram"></i>
-          </a>
-          <a href="https://www.linkedin.com">
-            <i class="fab fa-linkedin"></i>
-          </a>
+          <a href="https://twitter.com">Twitter</a>
+          <a href="https://www.facebook.com">Facebook</a>
+          <a href="https://www.instagram.com">Instagram</a>
+          <a href="https://www.linkedin.com">LinkedIn</a>
         </p>
       </footer>
     );
