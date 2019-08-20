cc05574a0d17a5084fe46d062f1ea068f1953d38
mdelzotto <madelzotto@comcast.net>
2019-08-20 18:37:49 -0400

    footer icon alignment

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,16 +5,25 @@ class Footer extends Component {
   render() {
     return (
       <footer>
-        <p>
-          © 2019
-          <a href="https://lambdaschool.com/" target="_blank">Lambda School</a>
-          <a href="https://github.com/event-planner-lambda-labs" target="_blank"><i className="fab fa-github" /></a>
-          <a href="https://twitter.com" target="_blank"><i className="fab fa-twitter" /></a>
-          <a href="https://www.facebook.com" target="_blank"><i className="fab fa-facebook" /></a>
-          <a href="https://www.instagram.com" target="_blank"><i className="fab fa-instagram" /></a>
-          <a href="https://www.linkedin.com" target="_blank"><i className="fab fa-linkedin-in" />  </a>
-          
-        </p>
+        © 2019
+        <a href="https://lambdaschool.com/" target="_blank">
+          Lambda School
+        </a>
+        <a href="https://github.com/event-planner-lambda-labs" target="_blank">
+          <i className="fab fa-github" />
+        </a>
+        <a href="https://twitter.com" target="_blank">
+          <i className="fab fa-twitter" />
+        </a>
+        <a href="https://www.facebook.com" target="_blank">
+          <i className="fab fa-facebook" />
+        </a>
+        <a href="https://www.instagram.com" target="_blank">
+          <i className="fab fa-instagram" />
+        </a>
+        <a href="https://www.linkedin.com" target="_blank">
+          <i className="fab fa-linkedin-in" />{" "}
+        </a>
       </footer>
     );
   }
