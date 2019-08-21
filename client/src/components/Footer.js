68c68a2b1081dae523cced58c300e2cc5ecab3a1
Oliver-Strange <preston.burton@gmail.com>
2019-08-20 17:25:10 -0700

    adds some styling to the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,23 +5,25 @@ class Footer extends Component {
   render() {
     return (
       <footer>
+        <div className="footer-links">
+          <a href="https://lambdaschool.com/">Lambda School</a>
+          <a href="https://github.com/event-planner-lambda-labs">
+            <i className="fab fa-github" />
+          </a>
+          <a href="https://twitter.com">
+            <i className="fab fa-twitter" />
+          </a>
+          <a href="https://www.facebook.com">
+            <i className="fab fa-facebook" />
+          </a>
+          <a href="https://www.instagram.com">
+            <i className="fab fa-instagram" />
+          </a>
+          <a href="https://www.linkedin.com">
+            <i className="fab fa-linkedin-in" />
+          </a>
+        </div>
         <p>© 2019</p>
-        <a href="https://lambdaschool.com/">Lambda School</a>
-        <a href="https://github.com/event-planner-lambda-labs">
-          <i className="fab fa-github" />
-        </a>
-        <a href="https://twitter.com">
-          <i className="fab fa-twitter" />
-        </a>
-        <a href="https://www.facebook.com">
-          <i className="fab fa-facebook" />
-        </a>
-        <a href="https://www.instagram.com">
-          <i className="fab fa-instagram" />
-        </a>
-        <a href="https://www.linkedin.com">
-          <i className="fab fa-linkedin-in" />
-        </a>
       </footer>
     );
   }
