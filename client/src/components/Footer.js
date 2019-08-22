76312e465ff9a164978cda6bf73ae6f1b65d8ce4
Oliver-Strange <preston.burton@gmail.com>
2019-08-22 10:54:42 -0700

    footer links open in a new tab

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,25 +7,28 @@ class Footer extends Component {
       <footer>
         <div className="footer-links">
           <a href="https://lambdaschool.com/">Lambda School</a>
-          <a href="https://github.com/event-planner-lambda-labs">
+          <a
+            href="https://github.com/event-planner-lambda-labs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fab fa-github" />
           </a>
-          <a href="https://twitter.com">
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-twitter" />
           </a>
-          <a href="https://www.facebook.com">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-facebook" />
           </a>
-          <a href="https://www.instagram.com">
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-instagram" />
           </a>
-          <a href="https://www.linkedin.com">
+          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
             <i className="fab fa-linkedin-in" />
           </a>
         </div>
-        
+
         <p>© 2019</p>
-        
       </footer>
     );
   }
