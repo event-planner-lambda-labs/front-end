77a899049f0a1d8088cdc39aad37abc7e9ebf102
droneal11015 <droneal11015@gmail.com>
2019-08-23 12:47:15 -0400

    updated footer links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,21 +7,17 @@ class Footer extends Component {
       <footer>
         <div className="footer-links">
           <a href="https://lambdaschool.com/">Lambda School</a>
-          <a href="https://github.com/event-planner-lambda-labs">
-            <i className="fab fa-github" />
-          </a>
-          <a href="https://twitter.com">
+          
+          <a href="https://twitter.com/GoHaveFun2">
             <i className="fab fa-twitter" />
           </a>
           <a href="https://www.facebook.com">
             <i className="fab fa-facebook" />
           </a>
-          <a href="https://www.instagram.com">
+          <a href="https://www.instagram.com/gohavefun1">
             <i className="fab fa-instagram" />
           </a>
-          <a href="https://www.linkedin.com">
-            <i className="fab fa-linkedin-in" />
-          </a>
+          
         </div>
         
         <p>© 2019</p>
