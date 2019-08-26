04abf9c5714dd3dbd2b2faba193b5c33cc73346b
Oliver-Strange <preston.burton@gmail.com>
2019-08-26 16:51:57 -0700

    removes un needed code

--- client/src/components/map/LocationSearchComponent.js
+++ client/src/components/map/LocationSearchComponent.js
@@ -15,11 +15,9 @@ class LocationSearchInput extends React.Component {
     this.setState({ value: address });
     geocodeByAddress(address)
       .then(results => getLatLng(results[0]))
-      // .then(results => console.log(results))
       .then(latLng => {
         this.props.location(latLng.lat, latLng.lng);
       })
-      // .then()
       .catch(error => console.error("Error", error));
   };
 
