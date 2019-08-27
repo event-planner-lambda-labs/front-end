a33db05389b295d10976de634cf262b12e91139a
Oliver-Strange <preston.burton@gmail.com>
2019-08-27 07:54:09 -0700

    fixes pics routes

--- client/src/pictures/index.js
+++ client/src/pictures/index.js
@@ -1,25 +1,25 @@
-import Anne from './anne.png'
-import Dani from './dani.png'
-import David from './david.png'
-import EventIcon from './event-icon.png'
-import Fun from './fun.jpg'
-import Mike from './mike.png'
-import Preston from './preston.png'
-import Features from './features.jpeg'
-import Upcoming from './upcoming.jpeg'
-import Map from './map.png'
-import CurrentLocation from './map-marker.png'
+import Anne from "./anne.jpg";
+import Dani from "./dani.jpg";
+import David from "./david.jpg";
+import EventIcon from "./event-icon.png";
+import Fun from "./fun.jpg";
+import Mike from "./mike.jpg";
+import Preston from "./preston.jpg";
+import Features from "./features.jpeg";
+import Upcoming from "./upcoming.jpeg";
+import Map from "./map.jpg";
+import CurrentLocation from "./map-marker.png";
 
 export {
-    Anne,
-    Dani,
-    David,
-    EventIcon,
-    Fun,
-    Mike,
-    Preston,
-    Features,
-    Upcoming,
-    Map,
-    CurrentLocation
-}
+  Anne,
+  Dani,
+  David,
+  EventIcon,
+  Fun,
+  Mike,
+  Preston,
+  Features,
+  Upcoming,
+  Map,
+  CurrentLocation
+};
